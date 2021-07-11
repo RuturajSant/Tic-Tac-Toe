9aca716ab13415ca7e4d9c189fc2144f12209f8f
Ruturaj <sant.ruturaj2001@gmail.com>
2021-07-11 12:03:40 +0530

    added winner & game history

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,33 +1,8 @@
-import React, {useState} from 'react'
+import React from 'react'
 import Square from './Square'
 
-const Board = () => {
+const Board = ({board,handleSquareClick}) => {
 
-    const [board, setBoard] = useState(Array(9).fill(null));
-    const [isXNext, setIsXNext] = useState(false);
-
-    const handleSquareClick = (position)=>{
-
-        // if( board[position]){
-        //     return;
-        // }
-        setBoard((prev)=>{
-            return prev.map((square,pos)=>{
-                if(pos === position && square == null){
-
-                    if(square == null){ 
-                        setIsXNext((prev)=> !prev);
-                        }else{
-                            setIsXNext((prev)=> prev); 
-                        }
-                        
-                    return isXNext ? 'X' : 'O';
-                }
-                return square;
-            });
-        });
-        
-    };
     const renderSquare = (position)=>{
         return <Square value={board[position]} onClick={()=>{handleSquareClick(position)}} />
     }
---
+++ src/helpers.js
@@ -0,0 +1,19 @@
+export function calculateWinner(squares){
+    const lines=[
+        [0,1,2],  //horizontal
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],  //vertical
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],  //diagonals
+        [2,4,6],
+    ];
+    for (let i=0; i<lines.length; i++){
+        const [a,b,c] =lines[i];   //iterates all the winning combinations array and holds values in a b c
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c] ){   // if all values in winning combination array a b c is equal then works
+            return squares[a];  //returns X or O (to be used as winner)
+        }
+    }
+    return null;
+}
