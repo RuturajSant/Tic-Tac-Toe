688a0f0fb856f2e21ec8555b3cb45619ca135724
Ruturaj <sant.ruturaj2001@gmail.com>
2021-07-11 12:34:17 +0530

    displayed history

---
+++ src/components/History.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const History = ({ history, moveTo, currentMove }) => {
+    return (
+        <ul>
+        {
+            history.map((_, move)=>{
+              return (
+                  <li key={move}>
+                        <button
+                         style = {{
+                            fontWeight:move===currentMove ? 'bold':'normal',
+                         }}
+                         onClick={()=>{
+                            moveTo(move);
+                        }} type="button">{move === 0 ? 'Lets Play!!' : `Go to move #${move}`}</button>
+                     </li>
+                     );
+            })}
+        </ul>
+    );
+};
+
+export default History
